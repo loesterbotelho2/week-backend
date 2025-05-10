a8d9c2593ca159163bed0592db32472422d2e89e
Loester Botelho <loesterfrancob@gmail.com>
2025-05-10 02:17:24 -0300

    Ajuste build

--- src/main/java/com/nexuscare/recipe/config/AppConfig.java
+++ src/main/java/com/nexuscare/recipe/config/AppConfig.java
@@ -1,11 +1,13 @@
 package com.nexuscare.recipe.config;
 
-import io.github.cdimascio.dotenv.Dotenv;
+//import io.github.cdimascio.dotenv.Dotenv;
 
 public class AppConfig {
+	/*
     private static final Dotenv dotenv = Dotenv.load();
 
     public static final String DB_URL = dotenv.get("DATABASE_URL");
     public static final String DB_USER = dotenv.get("DATABASE_USER");
     public static final String DB_PASSWORD = dotenv.get("DATABASE_PASSWORD");
+    */
 }
