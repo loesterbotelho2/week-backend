efa462c2cef2c83741c90076ddb2b77ab1f2039f
Loester Botelho <loesterfrancob3@gmail.com>
2025-05-10 12:53:31 -0300

    Update CorsConfig.java

--- src/main/java/com/nexuscare/recipe/config/CorsConfig.java
+++ src/main/java/com/nexuscare/recipe/config/CorsConfig.java
@@ -1,4 +1,4 @@
-package seu.pacote.config;
+package com.nexuscare.recipe.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
